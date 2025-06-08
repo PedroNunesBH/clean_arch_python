205efcb5faf5634bd4ed444d895f88906c3c11c7
Pedro Nunes <pedrohenriquestore99@gmail.com>
2025-06-08 15:38:01 -0300

    feat: add LoanEligibilityService domain service

---
+++ src/domain/services/loan_eligibility_service.py
@@ -0,0 +1,26 @@
+from src.domain.entities.loan_applicant import LoanApplicant
+
+
+class LoanEligibilityService:
+
+    @staticmethod
+    def is_eligible(loan_applicant: LoanApplicant) -> bool:
+        return (
+            loan_applicant.age >= 18
+            and loan_applicant.monthly_income >= 2000
+            and loan_applicant.score >= 600
+        )
+
+    @staticmethod
+    def maximum_value_allowed(loan_applicant: LoanApplicant) -> float:
+        if loan_applicant.score >= 800:
+            return loan_applicant.monthly_income * 10
+
+        elif loan_applicant.score >= 700:
+            return loan_applicant.monthly_income * 6
+
+        elif loan_applicant.score >= 600:
+            return loan_applicant.monthly_income * 3
+
+        else:
+            return 0.0
